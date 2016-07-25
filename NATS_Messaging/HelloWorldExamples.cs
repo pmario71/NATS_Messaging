284a0e4d767895ee61277d8ee6bdd5d8671fe35a
pmario71 <mario.plendl@web.de>
2016-07-25 09:19:16 +0200

    Cleanup of NATs example, still not fully functional.

---
+++ NATS_Messaging/HelloWorldExamples.cs
@@ -0,0 +1,88 @@
+﻿using NATS.Client;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NATS_Client
+{
+    [TestFixture]
+    public class HelloWorldExamples
+    {
+        private const int nrOfIterations = 300;
+        
+        [Test]
+        public void HelloWorld()
+        {
+            EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var receiver = new Receiver(waitHandle);
+
+            receiver.CaptureThreadID();
+            Console.WriteLine($"ThreadContext Main: {Thread.CurrentThread.ManagedThreadId}");
+
+            // Create a new connection factory to create
+            // a connection.
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+
+            // Creates a live connection to the default
+            // NATS Server running locally
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            Console.WriteLine(opts.Url);
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+
+                // Alternatively, create an asynchronous subscriber on subject foo, 
+                // assign a message handler, then start the subscriber.   When
+                // multicasting delegates, this allows all message handlers
+                // to be setup before messages start arriving.
+                IAsyncSubscription sAsync = connection.SubscribeAsync("foo");
+                sAsync.MessageHandler += receiver.MessageHandler;
+                sAsync.Start();
+
+                var sw = Stopwatch.StartNew();
+
+                Console.WriteLine("Start publishing ...");
+                for (int i = 0; i < nrOfIterations; i++)
+                {
+                    connection.Publish("foo", Encoding.UTF8.GetBytes("hello world " + i.ToString()));
+                }
+
+                connection.Publish("foo", Receiver.EndMark);
+                Console.WriteLine("Publish finished!");
+
+                sw.Stop();
+                Console.WriteLine("Sending {0} messages took: {1} ms", nrOfIterations, sw.ElapsedMilliseconds);
+                Console.WriteLine("Message deliverd on {0} threads.", receiver.Threads.Length);
+
+                if (!waitHandle.WaitOne(10000))
+                {
+                    Console.WriteLine("Not all messages were delivered within 5s!");
+                    Assert.Fail($"Number of messages processd: {receiver.Counter} / {nrOfIterations}");
+                }
+
+                // Unsubscribing
+                //sAsync.Unsubscribe();
+
+                //connection.Flush();
+
+                //// Publish requests to the given reply subject:
+                //connection.Publish("foo", "bar", Encoding.UTF8.GetBytes("help!"));
+
+                //// Sends a request (internally creates an inbox) and Auto-Unsubscribe the
+                //// internal subscriber, which means that the subscriber is unsubscribed
+                //// when receiving the first response from potentially many repliers.
+                //// This call will wait for the reply for up to 1000 milliseconds (1 second).
+                //m = connection.Request("foo", Encoding.UTF8.GetBytes("help"), 1000);
+
+                // Closing a connection
+            }
+        }
+    }
+}
---
+++ NATS_Messaging/Receiver.cs
@@ -0,0 +1,68 @@
+﻿using NATS.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NATS_Client
+{
+    class Receiver
+    {
+        private HashSet<int> _threadID = new HashSet<int>();
+        private int _counter;
+        private EventWaitHandle _handle;
+
+        public Receiver(EventWaitHandle handle)
+        {
+            Contract.Requires<ArgumentNullException>(handle != null);
+            _handle = handle;
+        }
+
+        public void MessageHandler(object sender, MsgHandlerEventArgs e)
+        {
+            //Console.WriteLine($"ThreadContext MessageHandler: {Thread.CurrentThread.ManagedThreadId}");
+
+            CaptureThreadID();
+            if ((++_counter) % 100 == 0)
+            {
+                Console.WriteLine("Received messages: {0}", _counter);
+            }
+            //Console.WriteLine("  > Message received:  ", e.Message.Subject);
+
+            if (ByteArrayHelper.ByteArrayCompare(e.Message.Data, EndMark))
+            {
+                Console.WriteLine("All messages received!");
+                _handle.Set();
+            }
+        }
+
+        public static readonly byte[] EndMark = new byte[] { 0 };
+
+        public int[] Threads
+        {
+            get
+            {
+                return _threadID.ToArray();
+            }
+        }
+
+        public int Counter
+        {
+            get
+            {
+                return _counter;
+            }
+        }
+
+        public void CaptureThreadID(string context = null)
+        {
+            var threadID = Thread.CurrentThread.ManagedThreadId;
+
+            if (!_threadID.Contains(threadID))
+                _threadID.Add(threadID);
+        }
+    }
+}
